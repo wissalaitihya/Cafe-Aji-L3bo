e8ae8b980c440c1b6c39eb8e6a6a603c36ecc0b2
Mimi-elmo <elmoaouine.mariam@gmail.com>
2026-04-17 09:48:51 +0100

    update the view

---
+++ app/View/error/404.php
@@ -0,0 +1,9 @@
+<?php require __DIR__ . '/../layout/header.php'; ?>
+
+<div class="error-page">
+    <h1>404</h1>
+    <p>Page not found</p>
+    <a href="/Cafe-Aji-L3bo/" class="btn">Go Home</a>
+</div>
+
+<?php require __DIR__ . '/../layout/footer.php'; ?>
